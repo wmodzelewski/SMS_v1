2c3ed31a4c4e61a34a4887eed58cc1f43a7a0990
waldemar.modzelewski <waldemar.modzelewski@cgi.com>
2017-10-21 15:57:54 +0200

    license service

---
+++ STS_0.0.1/src/main/java/org/wmodzelewski/sts/controllers/LicenseServiceController.java
@@ -0,0 +1,19 @@
+package org.wmodzelewski.sts.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.wmodzelewski.sts.models.License;
+
+@RestController
+@RequestMapping(value = "v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+	@RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+	License getLicenses(@PathVariable("organizationId") String organizationId,
+			@PathVariable("licenseId") String licenseId) {
+		return new License().withId(licenseId).withLicenseType("Seat").withProductName("Teleco")
+				.withOrganizationId(organizationId);
+	}
+}
