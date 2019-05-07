cdeca21099a907b863538765480f82e7b5387480
Waldemar Modzelewski <waldemar.modzelewski@cgi.com>
2019-05-07 15:06:02 +0200

    first scripts

---
+++ AZ-300/createCosmosDB.ps1
@@ -0,0 +1,39 @@
+﻿#check if logged in
+
+& "$PSScriptRoot\login.ps1"
+
+$resourceGroupName = "rgCosmosDb"
+$location = "northeurope"
+
+$tags = @{
+    "author" = "Waldemar Modzelewski";
+    "script" = "createCosmosDB.ps1"
+}
+
+$cosmosAccountName = "wmcosmos1"
+
+$cosmosLocations = @(
+    @{ "locationName"="northeurope"; "failoverPriority"=0 },
+    @{ "locationName"="westeurope"; "failoverPriority"=1 }
+)
+
+$consistencyPolicy = @{
+    "defaultConsistencyLevel"="BoundedStaleness";
+    "maxIntervalInSeconds"=300;
+    "maxStalenessPrefix"=100000
+}
+
+$CosmosDBProperties = @{
+    "databaseAccountOfferType"="Standard";
+    "locations"=$cosmosLocations;
+    "consistencyPolicy"=$consistencyPolicy;
+    "enableMultipleWriteLocations"="true"
+}
+
+New-AzResourceGroup -Location $location -Name $resourceGroupName -Tags $tags
+
+New-AzResource -ResourceType "Microsoft.DocumentDb/databaseAccounts" `
+    -ApiVersion "2015-04-08" -ResourceGroupName $resourceGroupName -Location $location `
+    -Name $cosmosAccountName -PropertyObject $CosmosDBProperties -Tags $tags -Force
+
+ 
---
+++ AZ-300/login.ps1
@@ -0,0 +1,20 @@
+﻿
+
+$context = Get-AzContext
+
+if($context) {
+    $needLogin = ([string]::IsNullOrEmpty($context.Account))
+
+    if($needLogin)
+    {
+        Connect-AzAccount
+    }
+    else {
+        "Already logged in"
+    } 
+}
+else {
+    "No context"
+} 
+
+
